e31b7058bce32ea052a024fda5245081555d0ee5
aryantiwari12 <aryantiwari1289@gmail.com>
2022-12-21 17:18:38 +0530

    -all

--- components/events/events-search.tsx
+++ components/events/events-search.tsx
@@ -1,7 +1,12 @@
 const EventsSearch=(props:any)=>{
     return(
-        <>
-        </>
+        <form>
+            <div>
+                <div>
+                    <label htmlFor=""></label>
+                </div>
+            </div>
+        </form>
     )
 }
 export default EventsSearch;
